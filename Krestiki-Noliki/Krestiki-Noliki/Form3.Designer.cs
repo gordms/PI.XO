4ab957efed7c28bbeb303db88ba0db8af103c8f9
badrakova <32175990+badrakova@users.noreply.github.com>
2017-11-20 12:30:42 +0300

    Add files via upload

--- Krestiki-Noliki/Krestiki-Noliki/Form3.Designer.cs
+++ Krestiki-Noliki/Krestiki-Noliki/Form3.Designer.cs
@@ -29,34 +29,30 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(234, 20);
+            this.label1.Size = new System.Drawing.Size(325, 278);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Правила игры";
-            // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(26, 37);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(231, 76);
-            this.label2.TabIndex = 1;
-            this.label2.Text = ".....";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label2);
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(325, 278);
             this.Controls.Add(this.label1);
             this.Name = "Form3";
-            this.Text = "Form3";
+            this.Text = "Об игре";
             this.ResumeLayout(false);
 
         }
@@ -64,6 +60,5 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
